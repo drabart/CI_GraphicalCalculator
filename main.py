d18734b5318be9093fc2fb73c2cc8724d09adc28
Bartosz <48629752+drabart@users.noreply.github.com>
2020-09-05 13:28:04 +0200

    Add files via upload
    comments and warning fixes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import sys
-import pygame as pg
+
+
 import json
 
 from classes import *
 from constants import *
-
+from pygame import *
+from sys import exit
 
 '''
 TODO list
@@ -31,8 +32,8 @@ def loadNodes(file):
                 arr.append(Node(obj[0], obj[1], r, t, obj[3], obj[3], obj[4], obj[5]))
             except IndexError:
                 arr.append(Node(obj[0], obj[1], r, t, obj[3], obj[3], obj[4]))
-        obj = 0
-        del obj
+        if 'obj' in locals():
+            del obj
 
     return arr
 
@@ -57,14 +58,17 @@ def loadMenu():
 
 
 def main():
-    pg.init()
+    #  initializing pygame
+    init()
     scr = Screen(800, 600, (0, 0, 0))
-    clock = pg.time.Clock()
+    clock = time.Clock()
+
+    #  array holding all renderable objects
     objectArr = []
 
-    objectArr.append(InputBox(260, 50, 50, 50, text='lorem ipsum'))
-
+    #  loading menu objects
     objectArr += loadMenu()
+
     n = loadNodes('save1.json')
     if n:
         objectArr += n
@@ -72,8 +76,6 @@ def main():
     mx, my = -1, -1
     dmx, dmy = 0, 0
     SCI = 0
-
-    nodeCount = [0]
 
     draggedNode = (-1, -1, -1)  # type, structure, id
     selectedNode = (-1, -1, -1, -1, -1)  # type, structure, id, x, y
@@ -87,38 +89,38 @@ def main():
 
     while 1:
         moved = False
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                sys.exit()
-            if event.type == pg.MOUSEBUTTONDOWN:
-                if event.button == 1:
+        for eve in event.get():
+            if eve.type == QUIT:
+                exit()
+            if eve.type == MOUSEBUTTONDOWN:
+                if eve.button == 1:
                     rButtonDown = True
-            if event.type == pg.MOUSEBUTTONUP:
-                if event.button == 1:
+            if eve.type == MOUSEBUTTONUP:
+                if eve.button == 1:
                     rButtonDown = False
                 if draggedNode != (-1, -1, -1):
                     draggedNode = (-1, -1, -1)
-            if event.type == pg.MOUSEMOTION:
-                x, y = event.pos
+            if eve.type == MOUSEMOTION:
+                x, y = eve.pos
                 if draggedNode != (-1, -1, -1):
-                    if event.pos[0] < 250:
+                    if eve.pos[0] < 250:
                         x = 250
-                    if event.pos[1] < 120:
+                    if eve.pos[1] < 120:
                         y = 120
-                    pg.mouse.set_pos(x, y)
+                    mouse.set_pos(x, y)
 
                 dmx = x - mx
                 dmy = y - my
                 mx, my = x, y
                 moved = True
-            if event.type == pg.KEYDOWN:
+            if eve.type == KEYDOWN:
                 if selectedInput:
-                    if event.key == pg.K_RETURN:
+                    if eve.key == K_RETURN:
                         currentText = ''
-                    elif event.key == pg.K_BACKSPACE:
+                    elif eve.key == K_BACKSPACE:
                         currentText = currentText[:-1]
                     else:
-                        currentText += event.unicode
+                        currentText += eve.unicode
                     updatedText = True
 
         if not moved or skipMove:
@@ -129,77 +131,82 @@ def main():
         skipConnect = False
 
         for obj in objectArr:
+            #  Click check
             try:
+                #  Check for 'InputBoxes' to be unselected
+                if not obj.mouseOver() and rButtonDown:
+                    if type(obj) == 'InputBox' and selectedInput:
+                        selectedInput = False
+                        obj.clicked = False
+                #  Check if object is being clicked
+                if obj.mouseOver() and rButtonDown:
+                    #  Set objects clicked only if no items are added
+                    if draggedNode == (-1, -1, -1):
+                        #  Selecting input box
+                        if type(obj) == 'InputBox' and not selectedInput:
+                            obj.clicked = True
+                            currentText = obj.text
+                            selectedInput = True
+                        else:
+                            obj.clicked = True
+                #  'unclicking' object if it is not 'InputBox'
+                elif not (obj.mouseOver() and rButtonDown) and type(obj) != 'InputBox':
+                    obj.clicked = False
+            except AttributeError:
+                pass
+
+            #  Nodes
+            #  Handling dragged node
+            try:
+                #  'undragging' dragged node
                 if obj.d and draggedNode == (-1, -1, -1):
                     obj.d = False
                     selectedNode = (-1, -1, -1, -1, -1)
-                    #  print(obj.x, obj.y)
+                #  moving dragged node
                 elif obj.d:
                     obj.move(dmx, dmy)
             except AttributeError:
                 pass
 
-            if updatedText:
-                try:
-                    if obj.textTexture and obj.clicked:
-                        obj.textUpdate(currentText)
-                        updatedText = False
-                except AttributeError:
-                    pass
-
-            try:
-                if not obj.mouseOver() and rButtonDown:
-                    try:
-                        if obj.textTexture and selectedInput:
-                            selectedInput = False
-                            obj.clicked = False
-                    except AttributeError:
-                        pass
-                if obj.mouseOver() and rButtonDown:
-                    try:
-                        if obj.textTexture and not selectedInput:
-                            obj.clicked = True
-                            currentText = obj.text
-                            selectedInput = True
-                    except AttributeError:
-                        pass
-                    if draggedNode == (-1, -1, -1):
-                        obj.clicked = True
-                elif not (obj.mouseOver() and rButtonDown):
-                    try:
-                        if obj.textTexture:
-                            pass
-                    except AttributeError:
-                        obj.clicked = False
-            except AttributeError:
-                pass
-
-            try:
-                if obj.name == 'Add_node' and obj.clicked and draggedNode == (-1, -1, -1):
-                    pg.mouse.set_pos(475, 360)
-                    newNode = Node(475, 360, 12, BASE_NODE_STR, SCI, nodeCount[SCI])
-                    #  print((475, 360, 12, BASE_NODE_STR, SCI, nodeCount[SCI]))
-                    objectArr.append(newNode)
-                    newNode.d = True
-                    draggedNode = (0, SCI, nodeCount[SCI])
-                    nodeCount[SCI] += 1
-                    skipMove = True
-            except AttributeError:
-                pass
-
+            #  Connecting nodes
             if not skipConnect:
                 try:
+                    #  If no node is selected, selects one
                     if obj.type == BASE_NODE_STR and obj.clicked and selectedNode == (-1, -1, -1, -1, -1):
                         selectedNode = (0, obj.strID, obj.id, obj.x, obj.y)
-                        #  print(selectedNode)
+                    #  If node is selected and current clicked node is not that one then connects
                     elif obj.type == BASE_NODE_STR and obj.clicked and selectedNode != (0, obj.strID, obj.id, obj.x, obj.y):
                         objectArr.append(Line(selectedNode[3]+obj.r, selectedNode[4]+obj.r, obj.x+obj.r, obj.y+obj.r, (0, 0, 0), 5))
                         selectedNode = (-1, -1, -1, -1, -1)
                         skipConnect = True
-                        #  print(selectedNode)
                 except AttributeError:
                     pass
-        #  print()
+
+            #  Adding node
+            try:
+                #  Checking if 'Add_node' button is clicked and no node is added right then
+                if obj.name == 'Add_node' and obj.clicked and draggedNode == (-1, -1, -1):
+                    #  Moves cursor to middle of map
+                    mouse.set_pos(475, 360)
+                    #  Creates node object
+                    newNode = Node(475, 360, 12, BASE_NODE_STR, SCI, 0)
+                    #  Adds node object to render array
+                    objectArr.append(newNode)
+                    #  Setting values to know which node is currently added
+                    newNode.d = True
+                    draggedNode = (0, SCI, 0)
+                    skipMove = True
+            except AttributeError:
+                pass
+
+            #  Text
+            #  Updates currently selected text
+            if updatedText:
+                if type(obj) == 'InputBox' and obj.clicked:
+                    obj.textUpdate(currentText)
+                    updatedText = False
+
+        #  Updating screen
         dt = clock.tick(60)
         scr.update(objectArr, dt)
 
